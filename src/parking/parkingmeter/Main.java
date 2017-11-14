04ba72b2ef6ea06ee788ad21f1fc8b535f2554fb
Zielarz <zielinskim25@gmail.com>
2017-11-14 13:18:11 +0100

    Init

---
+++ src/parking/parkingmeter/Main.java
@@ -0,0 +1,7 @@
+package parking.parkingmeter;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/parking/server/Main.java
@@ -0,0 +1,7 @@
+package parking.server;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}

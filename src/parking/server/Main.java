723011066402a4606af29ce9485cf843ba8d574e
Marcin Zielinski <zielinskim25@gmail.com>
2017-11-16 14:12:12 +0100

    Added main application window

--- src/parking/server/Main.java
+++ src/parking/server/Main.java
@@ -1,53 +1,30 @@
 package parking.server;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+
 import javafx.stage.Stage;
-import parking.server.model.LoginDAO;
+import parking.server.controller.LoginPopupController;
 import parking.util.DBUtil;
 
-import java.io.IOException;
 
 public class Main extends Application {
-    // Stage containing current view ??
-    private Stage primaryStage;
 
-    //
-    private AnchorPane loginLayout;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        this.primaryStage = primaryStage;
-        showLoginWindow();
-        primaryStage.setTitle("Parking Server Login");
+    public void start(Stage loginStage) throws Exception {
 
-       // primaryStage.show();
+        LoginPopupController loginInstance = new LoginPopupController();
+
+        loginInstance.showLoginWindow(loginStage);
+
         DBUtil.dbConnect();
-        //System.out.println(LoginDAO.login());
+
     }
 
-    public void showLoginWindow() {
-        try {
-            // Init login layout
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(this.getClass().getResource("view/LoginPopup.fxml"));
-            loginLayout = loader.load();
-
-            // Show the scene
-            Scene scene = new Scene(loginLayout);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e) {
-            System.err.println("Error while showing login window");
-            e.printStackTrace();
-        }
-    }
 
 }
 
